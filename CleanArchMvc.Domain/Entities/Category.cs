738b2cdc86a5cfff3c2c6bb38f0a79c85eda0eb1
BrunaMarcelle <bruna.silva@vocedm.com.br>
2024-03-22 16:22:42 -0300

    Incluindo entities, validation e interfaces

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        public Category(string name, int id)
+        {
+            DomainExecptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExecptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+            DomainExecptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
+    }
+}
---
+++ CleanArchMvc.Domain/Validation/DomainExecptionValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Validation
+{
+    public class DomainExecptionValidation : Exception
+    {
+        public DomainExecptionValidation(string error) : base(error)
+        {
+
+        }
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+                throw new DomainExecptionValidation(error);
+        }
+    }
+}
